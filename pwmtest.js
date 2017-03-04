b477beb957101d7383bfdfb5afa34e395ba2a454
Jonathan <misterikkit@gmail.com>
2017-03-04 06:26:57 +0000

    cleanup

--- pwmtest.js
+++ pwmtest.js
@@ -7,16 +7,19 @@ rpio.pwmSetClockDivider(128);
 rpio.pwmSetRange(12, 3000);
 
 function go() {
-	// 1.3ms == 195
-	rpio.pwmSetData(12,   195);
-	timers.setTimeout(()=>{
-			// 1.7ms == 255
-			rpio.pwmSetData(12,   255);
-			timers.setTimeout(()=>{
-					// 1.5ms == 225
-					rpio.pwmSetData(12,   225);
-					}, 500);
-			}, 5000);
+  // 1.3ms == 195
+  console.log('12 195');
+  rpio.pwmSetData(12, 195);
+  timers.setTimeout(() => {
+    // 1.7ms == 255
+    console.log('12 255');
+    rpio.pwmSetData(12, 255);
+    timers.setTimeout(() => {
+      // 1.5ms == 225
+      console.log('12 225');
+      rpio.pwmSetData(12, 225);
+    }, 500);
+  }, 5000);
 }
 
 timers.setInterval(go, 10000);
