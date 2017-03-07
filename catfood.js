4b724c97ad057f5a8463edaa3c9c85cb88c2db3f
Jonathan <misterikkit@gmail.com>
2017-03-06 23:42:17 -0800

    add log to startup and sleep to run.sh

--- catfood.js
+++ catfood.js
@@ -1,5 +1,7 @@
 const hardware = require('./hardware.js');
 const schedule = require('node-schedule');
+// add timestamp to logs.
+require('log-timestamp')(() => new Date().toLocaleString() + ' %s');
 
 hardware.setup();
 
@@ -8,9 +10,10 @@ hardware.setup();
 //////////////////////
 
 // 6AM
-schedule.scheduleJob({hour: 6, minute:0}, hardware.dispense);
+schedule.scheduleJob({hour : 6, minute : 0}, hardware.dispense);
 // NOON
-schedule.scheduleJob({hour: 12, minute:0}, hardware.dispense);
+schedule.scheduleJob({hour : 12, minute : 0}, hardware.dispense);
 // 11PM
-schedule.scheduleJob({hour: 23, minute:0}, hardware.dispense);
+schedule.scheduleJob({hour : 23, minute : 0}, hardware.dispense);
 
+console.log('catfood startup complete');
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+sleep 1m # maybe it needs to wait?
 cd $(dirname $0)
 sudo node catfood | tee -a /home/pi/catfood.log
