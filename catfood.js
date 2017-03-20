9703264683cee009949afd2b4f191dd6a89a01bc
Jonathan <misterikkit@gmail.com>
2017-03-19 23:11:42 -0700

    feed once per hour instead of 60 >_<

--- catfood.js
+++ catfood.js
@@ -30,6 +30,7 @@ stillAlive();  // One keepalive to signal job start.
 // Recurrence rule for feedings at 6AM, NOON, and 11PM.
 const feedings = new schedule.RecurrenceRule();
 feedings.hour = [6, 12, 23];
+feedings.minute = 0;
 schedule.scheduleJob(feedings, hardware.dispense);
 
 console.log('catfood startup complete');
