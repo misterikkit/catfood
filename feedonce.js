120834584a188b486afc4afd192f54e55f1f8131
Jonathan <misterikkit@gmail.com>
2017-04-02 23:16:49 -0700

    helper program

---
+++ feedonce.js
@@ -0,0 +1,6 @@
+const hardware = require('./hardware.js');
+// add timestamp to logs.
+require('log-timestamp')(() => new Date().toLocaleString() + ' %s');
+
+hardware.setup();
+hardware.dispense();
