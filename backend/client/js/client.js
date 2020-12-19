77d67a41fd79e58df147c3d039e3bb92ba24e361
Jonathan Basseri <misterikkit@gmail.com>
2020-12-18 22:21:09 -0800

    render the schedule

---
+++ backend/client/js/client.js
@@ -0,0 +1,42 @@
+function fillSchedule(schedule) {
+    let view = $("#schedule");
+    view.html("");
+    schedule.forEach((t) => {
+        let item = $(`<li><a href="#">${fmtTime(t)}</a><a href="#popupLogin" data-rel="popup"></a></li>`);
+        item.find("a").last()
+            .attr("time", JSON.stringify(t))
+            .click(editSchedule);
+        view.append(item);
+    });
+    let add = $("<li data-icon='plus'><a href='#'>add to the schedule</a></li>");
+    add.find("a").click(addSchedule);
+    view.append(add);
+    view.listview("refresh");
+}
+
+function fmtTime(t) {
+    let h = `${t.H}`;
+    let m = `${t.M}`;
+    // I hate that I have to do this
+    if (h.length === 1) { h = '0' + h; }
+    if (m.length === 1) { m = '0' + m; }
+    return `${h}:${m}`
+}
+
+function editSchedule() {
+
+}
+
+function addSchedule() {
+
+}
+
+function init() {
+    $.get('/config')
+        .fail(console.error)
+        .done((config) => {
+            fillSchedule(config.schedule);
+        });
+}
+
+$(init);
