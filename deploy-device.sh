c58055b18486e957ad41fdf2d6ad7cc619551ae3
Jonathan Basseri <misterikkit@gmail.com>
2020-12-21 22:17:11 -0800

    create device deploy script

---
+++ deploy-device.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -x
+
+TARGET="pi@10.0.0.3"
+VERSION="$(git describe --always --dirty)"
+
+cd $(dirname $0)
+
+rsync -az --progress --exclude node_modules device/ "${TARGET}:catfood-${VERSION}"
+# ssh $TARGET bash -c "rm catfood && ln -s catfood-${VERSION} catfood"
+
+ssh $TARGET << EOF
+set -x
+rm -f catfood
+ln -s catfood-${VERSION} catfood
+cd catfood
+npm install
+EOF
