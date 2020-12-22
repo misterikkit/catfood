e1149b323fd71825873d12b8eba2cc2f61dbb95a
Jonathan Basseri <misterikkit@gmail.com>
2020-12-21 23:01:16 -0800

    fix websocket message processing

--- device/cloud.js
+++ device/cloud.js
@@ -4,25 +4,27 @@ require('log-timestamp')(() => new Date().toLocaleString() + ' %s');
 
 
 function Connect(emitter) {
-    console.log('Connecting to cloud');
+    console.log('Cloud connecting');
     const ws = new WebSocket('ws://10.0.0.13:3000/device');
 
     ws.on('open', function open() {
-        console.log('Connected to cloud');
+        console.log('Cloud connected');
     });
 
     ws.on('error', (err) => {
-        console.log('Socket error', err);
+        console.log('Cloud socket error', err);
         ws.close();
     });
 
     ws.on('close', () => {
-        console.log('Disconnected');
+        console.log('Cloud disconnected');
         setTimeout(() => { Connect(emitter); }, 500);
     });
 
-    ws.on('message', function incoming(data) {
-        emitter.send(data.type, data.config);
+    ws.on('message', (data) => {
+        console.log('Cloud data received')
+        const msg = JSON.parse(data)
+        emitter.emit(data.type, data.config);
     });
 }
 
