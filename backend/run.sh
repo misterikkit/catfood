72d1d3cb5f26cd46da7316d10547f9831f7fc271
Jonathan Basseri <misterikkit@gmail.com>
2020-12-23 12:07:35 -0800

    fixes for run scripts

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 cd $(dirname $0)
+source ../.nvm/nvm.sh
+echo "using $(which node)"
 node index.js
--- device/run.sh
+++ device/run.sh
@@ -4,11 +4,14 @@ source /home/pi/catfood.env
 # Device seems to need some uptime before gpio works.
 while true; do
     upSeconds=$(cat /proc/uptime | cut -d'.' -f1)
-    if [[ "$upSeconds" > 60 ]]; then
+    if [[ "$upSeconds" -gt 60 ]]; then
         break
     fi
     sleep 1
 done
 uptime -p
 cd $(dirname $0)
-sudo CLOUD_TARGET="$CLOUD_TARGET" node catfood
+source ../.nvm/nvm.sh
+n=$(which node)
+echo "using $n"
+sudo CLOUD_TARGET="$CLOUD_TARGET" "$n" catfood
