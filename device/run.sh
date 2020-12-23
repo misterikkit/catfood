9693735ec8ef8415f3ac97b99a31df5b64d5b4de
Jonathan Basseri <misterikkit@gmail.com>
2020-12-22 21:45:04 -0800

    Properly pass env through run.sh
    (cherry picked from commit e04c3226067dfdc97879e3c8d3e411c85c4e4336)

--- device/run.sh
+++ device/run.sh
@@ -3,4 +3,4 @@
 source /home/pi/catfood.env
 sleep 1m # maybe it needs to wait?
 cd $(dirname $0)
-sudo node catfood | tee -a /home/pi/catfood.log
+sudo CLOUD_TARGET="$CLOUD_TARGET" node catfood | tee -a /home/pi/catfood.log
