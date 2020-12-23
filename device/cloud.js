2765e8144f227f2b10e3ead509250a6d042f3ddb
Jonathan Basseri <misterikkit@gmail.com>
2020-12-22 21:19:06 -0800

    log cloud target when connecting
    (cherry picked from commit 8d8788fbd769fc490819f3038452f29ab86489e2)

--- device/cloud.js
+++ device/cloud.js
@@ -4,7 +4,7 @@ require('log-timestamp')(() => new Date().toLocaleString() + ' %s');
 
 const target = process.env.CLOUD_TARGET || 'ws://10.0.0.13:3000/device';
 function Connect(emitter) {
-    console.log('Cloud connecting');
+    console.log('Cloud connecting to', target);
     const ws = new WebSocket(target);
 
     ws.on('open', function open() {
